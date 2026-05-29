809ea33f9588c65f6b934be8dff63c5d401be68b
camilaortiz962 <mortizh@unbosque.edu.co>
2026-05-28 22:18:03 -0500

    commit validacion nombre

--- InicioS.js
+++ InicioS.js
@@ -9,3 +9,15 @@ const errorNombre = document.getElementById("errorNombre");
 const errorCorreo = document.getElementById("errorCorreo");
 const errorPassword = document.getElementById("errorPassword");
 const mensajeVerificacion = document.getElementById("mensajeVerificacion");
+
+nombre.addEventListener("input", () => {
+
+    nombre.value = nombre.value.replace(/[^a-zA-ZáéíóúÁÉÍÓÚñÑ\s]/g, "");
+
+    if(nombre.value.length < 5){
+        errorNombre.textContent = "Debe ingresar nombre y apellido";
+    }else{
+        errorNombre.textContent = "";
+    }
+
+});
