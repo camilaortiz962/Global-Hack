9edbd22cc6f7900d69ecc904fd48c79aaf89328a
camilaortiz962 <mortizh@unbosque.edu.co>
2026-05-28 22:16:50 -0500

    commit js, css

---
+++ InicioS.js
@@ -0,0 +1,11 @@
+const formulario = document.getElementById("formulario");
+
+const nombre = document.getElementById("nombre");
+const correo = document.getElementById("correo");
+const password = document.getElementById("password");
+const verificarPassword = document.getElementById("verificarPassword");
+
+const errorNombre = document.getElementById("errorNombre");
+const errorCorreo = document.getElementById("errorCorreo");
+const errorPassword = document.getElementById("errorPassword");
+const mensajeVerificacion = document.getElementById("mensajeVerificacion");
