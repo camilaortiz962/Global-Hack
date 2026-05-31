10c21ba746a17e41d8d93cfab5a761a37d52bbdc
SaraPerilla <sperillac@unbosque.edu.co>
2026-05-31 16:55:59 -0500

    correciones carrito 1

--- scripts/Carrito.js
+++ scripts/Carrito.js
@@ -36,13 +36,3 @@ if (botonAceptar) {
 }
 
 
-}
-
-const boton = document.getElementById("botonAceptar");
-
-boton.addEventListener("click", () => {
-
-    alert("Confirmado correctamente");
-
-    });
-
